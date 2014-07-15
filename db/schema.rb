9eefd220bcebe3615304f714405d499017faf52c
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-07-15 16:29:09 +1000

    added UserCompanyRel model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140715062652) do
+ActiveRecord::Schema.define(:version => 20140715062818) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "user_company_rels", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "company_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
---
+++ test/unit/user_company_rel_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserCompanyRelTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
