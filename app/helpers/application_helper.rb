710a30121222e2f57a030dde353e444bf2da23c4
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-07-15 16:39:14 +1000

    added dummy auth system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def sign_in(user)
+    session[:current_user_id] = user.id
+  end
+  
+  def current_user
+    @current_user ||= User.find_by_id(session[:current_user_id])
+    return @current_user
+  end
+  
+  def sign_out()
+    session.delete(:current_user_id)
+  end
 end
