a977f6545df8795706cdf5bd45b366b1f4cf8480
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-07-15 16:38:51 +1000

    added ctrl for static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  # GET /
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DemoappRails::Application.routes.draw do
+  root to: 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
